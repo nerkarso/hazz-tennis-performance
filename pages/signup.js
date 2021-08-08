8fe9cb832cd7bd4948d0b66b54cbed15a1cc2a36
Ner Karso <nerpkarso@gmail.com>
2021-08-08 22:39:17 +0530

    Update sign up page

--- pages/signup.js
+++ pages/signup.js
@@ -1,79 +1,74 @@
 function Signup() {
   return (
-    <div
-      className="absolute inset-0 flex items-center justify-center bg-no-repeat bg-cover"
-      style={{
-        backgroundImage:
-          'url("https://images.unsplash.com/photo-1542446633-362158ea0052?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=891&q=80")',
-      }}>
-      <div className="w-full">
-        <div className="flex mx-auto overflow-hidden bg-white rounded-lg shadow dark:bg-base-800 lg:max-w-md">
-          <div className="w-full px-6 py-8 md:px-8">
-            <h2 className="text-2xl font-semibold text-center text-base-700 dark:text-white mb-2">
-              Hazz Tennis Performance
-            </h2>
-            <p className="text-xl text-center text-base-600 dark:text-base-200">Create your account</p>
-            <div className="mt-4">
-              <label className="block mb-2 text-sm font-medium text-base-600 dark:text-base-200" htmlFor="FullName">
-                Full name
+    <div className="flex items-center min-h-screen p-6 bg-base-50 dark:bg-base-900">
+      <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-base-800">
+        <div className="flex flex-col overflow-y-auto md:flex-row">
+          <div className="h-32 md:h-auto md:w-1/2">
+            <img
+              aria-hidden="true"
+              className="object-cover w-full h-full dark:hidden"
+              src={
+                'https://images.unsplash.com/photo-1542144582-1ba00456b5e3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=413&q=80'
+              }
+              alt="Office"
+            />
+          </div>
+          <div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+            <div className="w-full">
+              <h1 className="font-bold mb-2 text-primary-500">Hazz Tennis Peformance</h1>
+              <h1 className="mb-4 text-2xl font-semibold text-base-700 dark:text-base-200">Create account</h1>
+              <label className="block text-sm">
+                <span className="text-base-700 dark:text-base-400">Full name</span>
+                <input
+                  type="text"
+                  className="rounded-md block w-full mt-1 text-sm dark:border-base-600 dark:bg-base-700 focus:border-primary-400 focus:outline-none focus:shadow-outline-primary dark:text-base-300 dark:focus:shadow-outline-base form-input"
+                  placeholder="Maria Sharapova"
+                />
               </label>
-              <input
-                id="FullName"
-                className="block w-full px-4 py-2 text-base-700 bg-white border border-base-300 rounded-md dark:bg-base-800 dark:text-base-300 dark:border-base-600 focus:border-primary-500 dark:focus:border-primary-500 focus:outline-none focus:ring focus:ring-primary-300"
-                type="text"
-                placeholder="Maria Sharapova"
-              />
-            </div>
-            <div className="mt-4">
-              <label className="block mb-2 text-sm font-medium text-base-600 dark:text-base-200" htmlFor="EmailAddress">
-                Email address
+              <label className="block mt-4 text-sm">
+                <span className="text-base-700 dark:text-base-400">Email</span>
+                <input
+                  type="email"
+                  className="rounded-md block w-full mt-1 text-sm dark:border-base-600 dark:bg-base-700 focus:border-primary-400 focus:outline-none focus:shadow-outline-primary dark:text-base-300 dark:focus:shadow-outline-base form-input"
+                  placeholder="maria@example.com"
+                />
               </label>
-              <input
-                id="EmailAddress"
-                className="block w-full px-4 py-2 text-base-700 bg-white border border-base-300 rounded-md dark:bg-base-800 dark:text-base-300 dark:border-base-600 focus:border-primary-500 dark:focus:border-primary-500 focus:outline-none focus:ring focus:ring-primary-300"
-                type="email"
-                placeholder="maria@example.com"
-              />
-            </div>
-            <div className="mt-4">
-              <div className="flex justify-between">
-                <label className="block mb-2 text-sm font-medium text-base-600 dark:text-base-200" htmlFor="Password">
-                  Password
+              <label className="block mt-4 text-sm">
+                <span className="text-base-700 dark:text-base-400">Password</span>
+                <input
+                  className="rounded-md block w-full mt-1 text-sm dark:border-base-600 dark:bg-base-700 focus:border-primary-400 focus:outline-none focus:shadow-outline-primary dark:text-base-300 dark:focus:shadow-outline-base form-input"
+                  placeholder="***************"
+                  type="password"
+                />
+              </label>
+              <label className="block mt-4 text-sm">
+                <span className="text-base-700 dark:text-base-400">Confirm password</span>
+                <input
+                  className="rounded-md block w-full mt-1 text-sm dark:border-base-600 dark:bg-base-700 focus:border-primary-400 focus:outline-none focus:shadow-outline-primary dark:text-base-300 dark:focus:shadow-outline-base form-input"
+                  placeholder="***************"
+                  type="password"
+                />
+              </label>
+              <div className="flex mt-6 text-sm">
+                <label className="flex items-center dark:text-base-400">
+                  <input
+                    type="checkbox"
+                    className="text-primary-600 rounded form-checkbox focus:border-primary-400 focus:outline-none focus:shadow-outline-primary dark:focus:shadow-outline-base"
+                  />
+                  <span className="ml-2">I agree to the terms of service</span>
                 </label>
               </div>
-              <input
-                id="Password"
-                className="block w-full px-4 py-2 text-base-700 bg-white border border-base-300 rounded-md dark:bg-base-800 dark:text-base-300 dark:border-base-600 focus:border-primary-500 dark:focus:border-primary-500 focus:outline-none focus:ring focus:ring-primary-300"
-                type="password"
-                placeholder="Your password"
-              />
-            </div>
-            <div className="mt-4">
-              <div className="flex justify-between">
-                <label
-                  className="block mb-2 text-sm font-medium text-base-600 dark:text-base-200"
-                  htmlFor="ConfirmPassword">
-                  Confirm password
-                </label>
-              </div>
-              <input
-                id="ConfirmPassword"
-                className="block w-full px-4 py-2 text-base-700 bg-white border border-base-300 rounded-md dark:bg-base-800 dark:text-base-300 dark:border-base-600 focus:border-primary-500 dark:focus:border-primary-500 focus:outline-none focus:ring focus:ring-primary-300"
-                type="password"
-                placeholder="Confirm your password"
-              />
-            </div>
-            <div className="mt-8">
-              <button className="uppercase font-medium w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-primary-700 rounded-md hover:bg-primary-600 focus:outline-none focus:bg-primary-600">
-                Sign up
-              </button>
-            </div>
-            <div className="flex items-center justify-between mt-4">
-              <span className="w-1/5 border-b dark:border-base-600 md:w-1/4" />
-              <a href="/login" className="text-xs text-base-500 uppercase dark:text-base-400 hover:underline">
-                or login
+              <a
+                className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-primary-600 border border-transparent rounded-lg active:bg-primary-600 hover:bg-primary-700 focus:outline-none focus:shadow-outline-primary"
+                href="/login">
+                Create account
               </a>
-              <span className="w-1/5 border-b dark:border-base-600 md:w-1/4" />
+              <hr className="my-6" />
+              <p>
+                <a className="text-sm font-medium text-primary-600 dark:text-primary-400 hover:underline" href="/login">
+                  Already have an account? Login
+                </a>
+              </p>
             </div>
           </div>
         </div>
