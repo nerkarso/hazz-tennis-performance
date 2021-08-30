805602edb37a4b0f6d33e7c43a749fa40f514d1e
Ner Karso <nerpkarso@gmail.com>
2021-08-30 20:23:35 +0530

    Delete unused components

--- components/EditCoachModal.js
+++
@@ -1,5 +0,0 @@
-import NewCoachModal from '@/components/NewCoachModal';
-
-export default function EditCoachModal({ isOpen, onClose }) {
-  return <NewCoachModal title="Edit Coach Details" isOpen={isOpen} onClose={onClose} btnText="Update" />;
-}
--- components/MessageFooter.js
+++
@@ -1,7 +0,0 @@
-export default function MessageFooter() {
-  return (
-    <div className="flex flex-grow-0 p-4 mt-2 h-15">
-      <input placeholder="Enter message then press enter" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-1/ focus:ring-primary-500 focus:border-primary-500 " cols="50" />
-    </div>
-  );
-}
