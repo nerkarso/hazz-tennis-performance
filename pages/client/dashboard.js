ab88f6211c19d746d08f290ce10d5b25b6e742ce
Ner Karso <nerpkarso@gmail.com>
2021-09-01 22:12:13 +0530

    Add bookings table on client dashboard page

--- pages/client/dashboard.js
+++ pages/client/dashboard.js
@@ -1,6 +1,26 @@
+import ClientBookingsTable from '@/components/ClientBookingsTable';
 import ClientSidebar from '@/components/ClientSidebar';
+import DashboardContent from '@/components/DashboardContent';
+import DashboardGreeting from '@/components/DashboardGreeting';
 import DashboardLayout from '@/components/DashboardLayout';
 
+Dashboard.title = 'Dashboard';
+
 export default function Dashboard() {
-  return <DashboardLayout sidebar={<ClientSidebar />}></DashboardLayout>;
+  return (
+    <DashboardLayout sidebar={<ClientSidebar />}>
+      <DashboardContent header={<DashboardGreeting firstName="Novak" lastName="Djokovic" role="Client" />}>
+        <RecentBookingsSection />
+      </DashboardContent>
+    </DashboardLayout>
+  );
+}
+
+function RecentBookingsSection() {
+  return (
+    <section>
+      <h1 className="mb-4 text-2xl font-bold">Recent bookings</h1>
+      <ClientBookingsTable rows={Array.from(Array(3))} />
+    </section>
+  );
 }
