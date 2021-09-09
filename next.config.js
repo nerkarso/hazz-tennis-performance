2b9dcf33870ced6a44a8e762343f65dd055a11e7
Ner Karso <nerpkarso@gmail.com>
2021-09-10 02:27:20 +0530

    Add redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/client',
+        destination: '/client/dashboard',
+        permanent: true,
+      },
+      {
+        source: '/coach',
+        destination: '/coach/dashboard',
+        permanent: true,
+      },
+      {
+        source: '/owner',
+        destination: '/owner/dashboard',
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
-}
+};
