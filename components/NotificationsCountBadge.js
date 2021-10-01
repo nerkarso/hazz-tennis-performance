fc411a40a9d75b2523d0129184e5cf14f0856358
Ner Karso <nerpkarso@gmail.com>
2021-10-02 01:41:48 +0530

    Fix notification socket

--- components/NotificationsCountBadge.js
+++ components/NotificationsCountBadge.js
@@ -14,7 +14,7 @@ export default function NotificationsCountBadge() {
       queryClient.invalidateQueries('notificationsCount');
     });
     // eslint-disable-next-line
-  }, []);
+  }, [socket]);
 
   if (data?.count > 0) {
     return <span className="absolute top-0 right-0 grid h-4 px-1 text-xs font-bold text-white translate-x-1 bg-red-600 rounded-full place-items-center dark:bg-red-500">{data?.count}</span>;
